9c187fcba8886fa0cf3b876c97a7f09a0e713a31
abdulShukor <ashukoor12@gmail.com>
2021-06-30 13:02:05 -0400

    Worked on input and address picker FS components

--- cypress/integration/examples/address_components_fs.js
+++ cypress/integration/examples/address_components_fs.js
@@ -3,13 +3,24 @@
 require("cypress-xpath");
 
 describe("address_components_firestitch", () => {
-    it("Test_address_components_firestitch!", () => {
-        cy.visit("http://address.components.firestitch.com/")
-        cy.get('#mat-input-0').type("Toronto")
-        cy.contains("Toronto, ON, Canada").click()
-        cy.get('kitchen-sink > .fs-form > .mat-focus-indicator > .mat-button-wrapper').click()
-        cy.contains("Saved")
-    
+  it("Address Picker!", () => {
+    cy.visit("http://address.components.firestitch.com/");
+    cy.get("#mat-input-0").type("Toronto");
+    cy.contains("Toronto, ON, Canada").click();
+    cy.contains("Save").dblclick();
+    cy.contains("Saved");
 
-    });
+    cy.get(".mat-form-field-infix.ng-tns-c108-0").click();
+
+    cy.get("#mat-input-27").dblclick().type("27");
+    cy.get("#mat-input-21").dblclick().type("21");
+    cy.get("#mat-input-22").dblclick().type("22");
+    cy.get("#mat-input-23").dblclick().type("23");
+    cy.get("#mat-input-24").dblclick().type("24");
+    cy.get("#mat-input-25").dblclick().type("25");
+    cy.get("#mat-input-26").dblclick().type("26");
+
+    //cy.get('#mat-dialog-0').dblclick().type("Markham")
+    cy.contains("Apply").click();
+  });
 });
