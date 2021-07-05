b0e589fe59cae396aa7f2057eec49cb895ce9c49
abdulShukor <ashukoor12@gmail.com>
2021-07-05 19:12:24 -0400

    Added negative test cases, Address Picker

--- cypress/integration/examples/address_components_fs.js
+++ cypress/integration/examples/address_components_fs.js
@@ -2,7 +2,7 @@
 /// <reference types="cypress-iframe" />
 
 describe("address_components_firestitch", () => {
-  it.only("Address Picker!", () => {
+  it("Address Picker!", () => {
     cy.visit("http://address.components.firestitch.com/");
 
     //Location
@@ -55,6 +55,16 @@ describe("address_components_firestitch", () => {
     cy.contains("Cancel").click()
     cy.wait(3000)
 
+    // two way binding
+    cy.get(".mat-form-field-infix.ng-tns-c108-0").click();
+    cy.get(".mat-input-element").eq("25").clear()
+    cy.get(".mat-input-element").eq("26").clear()
+    cy.get(".mat-input-element").eq("25").type("Ontario")
+    cy.contains("Ontario, Canada").click()
+    cy.get(".mat-input-element").eq("26").should("have.value", "Canada");
+    cy.contains("Apply").dblclick();
+
+
     // negative tests 
     cy.get(".mat-form-field-infix.ng-tns-c108-0").click();
     cy.get(".mat-input-element").eq("24").clear()
@@ -68,35 +78,39 @@ describe("address_components_firestitch", () => {
 
   });
 
-  it(" Confirmation Address Picker!", () => {
+    // Confirmation Address Picker
+  it.only(" Confirmation Address Picker!", () => {
     cy.visit("http://address.components.firestitch.com/");
 
 
     cy.contains("Save").should("be.disabled");
-    cy.get("#mat-input-1").click().type("Toronto");
+    cy.get(".mat-input-element").eq("1").click().type("Toronto");
     cy.contains("Toronto, ON, Canada").click();
 
     //Dialog box
 
-    cy.get("#mat-input-27").dblclick().type("215 Markham Road");
+  //cy.get(".mat-input-element").eq("21").dblclick().type("215 Markham Road");
 
-    cy.get("#mat-input-21").dblclick().type("2550 Lawrence");
-    cy.get("#mat-input-22").dblclick().type("1470 Midland");
+  
+  cy.get(".mat-input-element").eq("21").dblclick().type("2550 Lawrence");
+
+    cy.get(".mat-input-element").eq("22").dblclick().type("2550 Lawrence");
+    cy.get(".mat-input-element").eq("23").dblclick().type("1470 Midland");
 
     //Dialog box require fields
     // City require field
-    cy.get("#mat-input-23").should("have.prop", "required");
+    cy.get(".mat-input-element").eq("24").should("have.prop", "required");
     // Country require field
-    cy.get("#mat-input-24").should("have.prop", "required");
+    cy.get(".mat-input-element").eq("25").should("have.prop", "required");
     //Province require field
-    cy.get("#mat-input-25").should("have.prop", "required");
+    cy.get(".mat-input-element").eq("26").should("have.prop", "required");
     //cy.scrollTo('bottom')
 
     // assertion
-    cy.get("#mat-input-23").should("have.value", "Toronto");
-    cy.get("#mat-input-24").should("have.value", "Ontario");
-    cy.get("#mat-input-25").should("have.value", "Canada");
-    cy.get("#mat-input-26").dblclick().type("M1J 3C4");
+    cy.get(".mat-input-element").eq("24").should("have.value", "Toronto");
+    cy.get(".mat-input-element").eq("25").should("have.value", "Ontario");
+    cy.get(".mat-input-element").eq("26").should("have.value", "Canada");
+    cy.get(".mat-input-element").eq("27").dblclick().type("M1J 3C4");
 
     // Google Map scrolling up and down
     cy.contains("Google").scrollIntoView();
